d8852fe2a2040d7fe2fa4aebaa9234711581cc72
Neferith <vivien.bernard@gmail.com>
2022-11-08 21:06:23 +0100

    Corrections de bugs

--- app/src/main/java/com/frontparissportifs/repository/LeagueRepository.kt
+++ app/src/main/java/com/frontparissportifs/repository/LeagueRepository.kt
@@ -6,6 +6,7 @@ import com.frontparissportifs.network.league.LeagueApi
 import com.frontparissportifs.network.league.LeagueMapper
 import com.frontparissportifs.utils.DataState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
@@ -13,52 +14,48 @@ class LeagueRepository @Inject
 constructor(
     private val leagueApi: LeagueApi,
     private val leagueMapper: LeagueMapper,
-    private  val leagueDao:LeagueDao, private val
-    cacheMapper: CacheMapper){
+    private val leagueDao: LeagueDao, private val
+    cacheMapper: CacheMapper
+) {
 
     private var apiDataIsLoaded = false
 
     private suspend fun initializeDatabaseIfNeed() {
-        if(!apiDataIsLoaded) {
+        if (!apiDataIsLoaded) {
             val apiLeagues = leagueApi.getAllLeagues()
             val convertApiLeagues = leagueMapper.mapFromEntityList(apiLeagues.leagues)
             apiDataIsLoaded = true
             for (league in convertApiLeagues) {
-                if(league.sport.equals("Soccer")) {
+                if (league.sport.equals("Soccer")) {
                     leagueDao.insert(cacheMapper.mapToEntity(league))
                 }
             }
         }
     }
 
-    suspend fun foundAllSoccerLeagues(): Flow<DataState<List<String>>> = flow {
+    suspend fun foundAllSoccerLeagues(
+    ): Flow<DataState<List<String>>> = flow<DataState<List<String>>>{
         initializeDatabaseIfNeed()
-        try {
-            val cacheLeagues = leagueDao.getAll()
-            val leagues = cacheMapper.mapFromEntityListToListString(cacheLeagues)
-            emit(DataState.Success(leagues))
-        } catch (e: Exception) {
-            emit(DataState.Error(e))
-        }
-    }
 
-    suspend fun fetchIsLeagueExist(leagueName:String): Flow<DataState<Boolean>> = flow {
+        val cacheLeagues = leagueDao.getAll()
+        val leagues = cacheMapper.mapFromEntityListToListString(cacheLeagues)
+        emit(DataState.Success(leagues))
+
+    }.catch { e -> emit(DataState.Error(e))}
+
+    suspend fun fetchIsLeagueExist(
+        leagueName: String
+    ): Flow<DataState<Boolean>> = flow<DataState<Boolean>>{
+
         initializeDatabaseIfNeed()
-        try {
-
-            val leagues = leagueDao.getByKeywordEqual(leagueName)
-            if(leagues.isNotEmpty()) {
-                emit(DataState.Success(true))
-            } else {
-                emit(DataState.Success(false))
-            }
-
-
-        } catch (e: Exception) {
-            emit(DataState.Error(e))
+        val leagues = leagueDao.getByKeywordEqual(leagueName)
+        if (leagues.isNotEmpty()) {
+            emit(DataState.Success(true))
+        } else {
+            emit(DataState.Success(false))
         }
-    }
 
+    }.catch { e -> emit(DataState.Error(e))}
 
 
 }
--- app/src/main/java/com/frontparissportifs/utils/DisplayUtils.kt
+++ app/src/main/java/com/frontparissportifs/utils/DisplayUtils.kt
@@ -5,7 +5,7 @@ import android.content.res.Resources
 import android.widget.Toast
 
 fun displayError(context: Context, message: String?) {
-    if (message.isNullOrEmpty()) {
+    if (!message.isNullOrEmpty()) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     } else {
         Toast.makeText(context, "Unknown error", Toast.LENGTH_LONG).show()
