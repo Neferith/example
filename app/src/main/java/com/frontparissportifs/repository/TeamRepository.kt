0addbb053b9c27d6da8d97e5368126e10d175cb7
vivien <somite100>
2022-11-08 09:41:08 +0100

    - Fix catch in flow

--- app/src/main/java/com/frontparissportifs/repository/TeamRepository.kt
+++ app/src/main/java/com/frontparissportifs/repository/TeamRepository.kt
@@ -6,25 +6,23 @@ import com.frontparissportifs.network.team.TeamMapper
 import com.frontparissportifs.ui.base.ParameterException
 import com.frontparissportifs.utils.DataState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class TeamRepository @Inject
-constructor(private val teamApi: TeamApi, private val teamMapper: TeamMapper){
+constructor(private val teamApi: TeamApi, private val teamMapper: TeamMapper) {
 
-    suspend fun getByLeagues(leagueName:String?): Flow<DataState<List<Team>>> = flow {
+    suspend fun getByLeagues(leagueName: String?): Flow<DataState<List<Team>>> = flow {
         emit(DataState.Loading)
-        try {
-            if (leagueName == null) {
-                throw ParameterException("League name is null")
-            }
-            val apiTeams = teamApi.getByLeagues(leagueName)
-            val teams = teamMapper.mapFromEntityList(apiTeams.teams)
 
-            emit(DataState.Success(teams))
-        } catch (e: Exception) {
-            emit(DataState.Error(e))
+        if (leagueName == null) {
+            throw ParameterException("League name is null")
         }
-    }
+        val apiTeams = teamApi.getByLeagues(leagueName)
+        val teams = teamMapper.mapFromEntityList(apiTeams.teams)
+
+        emit(DataState.Success(teams))
+    }.catch { e: Throwable -> emit(DataState.Error(e)) }
 
 }
